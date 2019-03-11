d30be73feadf6f465970a54afa8064743a54b6db
ibrahim köprücü <i.koprucu07@gmail.com>
2019-03-12 00:43:01 +0300

    some changes

--- LinqQueryMethods/Program.cs
+++ LinqQueryMethods/Program.cs
@@ -8,13 +8,11 @@ namespace LinqQueryMethods
     private static void Main(string[] args)
     {
       int[] nums = { 1, -2, 3, 0, -4, 5 };
-      var posNums = nums.Where(n => n > 0).Select(r => r);
+      var posNums = nums.Where(n => n > 0).OrderByDescending(j => j);
       foreach (int i in posNums)
       {
         Console.Write(i + " ");
       }
-
-      Console.WriteLine("eee");
     }
   }
 }
---
+++ ToArrayMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ToArrayMethod
+{
+  public class Program
+  {
+    private static void Main(string[] args)
+    {
+      int[] nums = { 1, -2, 3, 0, -4, 5 };
+
+      var posNums = from n in nums
+                    where n > 0
+                    select n;
+      int len = posNums.Count();
+      Console.WriteLine(len);
+    }
+  }
+}
